d2e8f74dcedf88f5bdd59b09a37f343e4dc88fa6
Gayani Gunasekera <gayanimaheshi@gmail.com>
2018-07-24 12:49:09 +0100

    another commit on 24th July

--- js/hello.js
+++ js/hello.js
@@ -1,7 +1,7 @@
 
 var $ = require('jquery');
 $(function() { //start
-
+// ##########################################
 var x = document.getElementById("myTopnav");
  x.addEventListener('click',function(){
    if (this.className === "topnav") {
@@ -12,6 +12,37 @@ var x = document.getElementById("myTopnav");
    }
  },false)
 
+ // Activate the drop down manu item from drop down menu on Mobile mode***********
+   // var myClass = 'active';
+   //
+   // var ddEl = document.getElementById('hamburger');
+   // var list = document.querySelectorAll("#myTopnav a");
+   // for (var i = 0; i < list.length; i++) {
+   //   list[i].addEventListener("click", function() {
+   //     var curentclas = this.className;
+   //     // alert(curentclas);
+   //     var clk = document.querySelectorAll("." + myClass);
+   //     for (var i = 0; i < clk.length; i++) {
+   //       clk[i].className = '';
+   //     }
+   //    this.className = myClass;
+   //     // alert( myClass);
+   //   });
+   // }
+
+// for the Google map ###########################
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(52.1394923, -10.26537570000005),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+// ##############################################
 
 
+
+
+// ##########################################
 }); //end
