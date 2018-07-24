a68c0d91e193708996b0f643afd54760f463a046
Gayani Gunasekera <gayanimaheshi@gmail.com>
2018-07-24 13:52:21 +0100

    3rd commit for the day

--- js/hello.js
+++ js/hello.js
@@ -12,23 +12,6 @@ var x = document.getElementById("myTopnav");
    }
  },false)
 
- // Activate the drop down manu item from drop down menu on Mobile mode***********
-   // var myClass = 'active';
-   //
-   // var ddEl = document.getElementById('hamburger');
-   // var list = document.querySelectorAll("#myTopnav a");
-   // for (var i = 0; i < list.length; i++) {
-   //   list[i].addEventListener("click", function() {
-   //     var curentclas = this.className;
-   //     // alert(curentclas);
-   //     var clk = document.querySelectorAll("." + myClass);
-   //     for (var i = 0; i < clk.length; i++) {
-   //       clk[i].className = '';
-   //     }
-   //    this.className = myClass;
-   //     // alert( myClass);
-   //   });
-   // }
 
 // for the Google map ###########################
 function myMap() {
@@ -42,7 +25,4 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 // ##############################################
 
 
-
-
-// ##########################################
 }); //end
